080568f34e0b811cb059d851717456f8d4c6631c
Ammar Alqatari <ammar.qat@gmail.com>
2018-06-05 00:04:18 -0700

    changed tree to set of pointers to nodes

--- implementation/BDD.hpp
+++ implementation/BDD.hpp
@@ -18,11 +18,11 @@ struct Node {
     int value;
     Node* hi;
     Node* lo;
-    bool marked;
     bool operator ==(const Node & n) const {
         return (var == n.var && hi == n.hi && lo == n.lo);
     }
-
+    
+    Node(): id(0), var(0), value(0), hi(NULL), lo(NULL) {}
 };
 
 
@@ -33,8 +33,8 @@ struct hash<Node>
     size_t
     operator()(const Node & n) const
     {
-        size_t v1 = (n.lo)->var;
-        size_t v2 = (n.hi)->var;
+        size_t v1 = n.lo != NULL ? (n.lo)->var : -1;
+        size_t v2 = n.hi != NULL ? (n.hi)->var : -1;
         return hash<int>()((v1+v2)*(v1+v2+1)/2 + v1);
     }
     
@@ -43,22 +43,26 @@ struct hash<Node>
 
 
 class BDD {
+    friend ostream& operator<<(ostream& os, const BDD& bdd);
 public:
     explicit BDD();
-    
+    ~BDD();
     /* need to implement:
      - build(t): constructs a BDD using given boolean expression
      - and(bdd): AND's two bdds
      - satisfy_one
      */
     int getSize();
+    Node *conjunction(class BDD b);
+    Node *disjunction(class BDD b);
+    void addVariable(int num);
     
     
 private:
     int numVars;
-    vector<Node> Tree; //Vector of tree nodes
+    vector<Node*> Tree; //Vector of tree nodes
     unordered_map<Node, size_t> H; //Hash map from node to node index in vector
-    Node mk(size_t var, Node* hi, Node *lo); //Inserts node while making sure there are no duplicates.
+    Node *mk(size_t var, Node* hi, Node *lo); //Inserts node while making sure there are no duplicates.
     Node *apply(int op, BDD b);
     Node *applyHelper(int op, Node u1, Node u2, vector<vector<Node *>>& table);
     Node getRoot();
